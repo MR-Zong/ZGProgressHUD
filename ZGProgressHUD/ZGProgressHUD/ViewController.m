b993a95f1e9e9f64d3ad474701e9742f39d9e894
ZongGenXu <540682674@qq.com>
2016-12-04 10:29:51 +0800

    效果展示

--- ZGProgressHUD/ZGProgressHUD/ViewController.m
+++ ZGProgressHUD/ZGProgressHUD/ViewController.m
@@ -19,8 +19,32 @@
     [super viewDidLoad];
     
     // Example:Loading
-//    [ZGProgressHUD showInView:self.view message:@"最大输入字数不能超过140~" mode:ZGProgressHUDModeText];
+    UIButton *btn1 = [UIButton buttonWithType:UIButtonTypeCustom];
+    btn1.tag = 10;
+    btn1.frame = CGRectMake(50, 440, 120, 60);
+    [btn1 setTitle:@"网络加载样例" forState:UIControlStateNormal];
+    btn1.backgroundColor = [UIColor orangeColor];
+    [btn1 addTarget:self action:@selector(didTouchButton:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:btn1];
     
+    UIButton *btn2 = [UIButton buttonWithType:UIButtonTypeCustom];
+    btn2.tag = 20;
+    btn2.frame = CGRectMake(CGRectGetMaxX(btn1.frame) + 20, 440, 120, 60);
+    [btn2 setTitle:@"toast样例" forState:UIControlStateNormal];
+    btn2.backgroundColor = [UIColor purpleColor];
+    [btn2 addTarget:self action:@selector(didTouchButton:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:btn2];
+
+    
+}
+
+- (void)didTouchButton:(UIButton *)btn
+{
+    if (btn.tag == 10) {
+        [self loadData];
+    }else if(btn.tag == 20){
+            [ZGProgressHUD showInView:self.view message:@"最大输入字数不能超过140~" mode:ZGProgressHUDModeToast];
+    }
 }
 
 - (void)loadData
@@ -34,18 +58,5 @@
     });
 }
 
-- (void)example
-{
-    [self loadData];
-//    [ZGProgressHUD showInView:self.view message:@"最大输入字数不能超过140~" mode:ZGProgressHUDModeToast];
-
-}
-
-- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
-{
-    [super touchesBegan:touches withEvent:event];
-    
-    [self example];
-}
 
 @end
